2df9eff003a38ca7beb17caad30f9af24a8f6d25
Sean Larimore <slarimore02@gmail.com>
2023-02-28 06:35:17 +0000

    update description for cluster_ip

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "controller_ha" {
   default     = "false"
 }
 variable "cluster_ip" {
-  description = "Optional sets the IP address of the Avi Controller cluster. This address must be in the same subnet as the Avi Controller VMs."
+  description = "Sets the IP address of the Avi Controller cluster. This address must be in the same subnet as the Avi Controller VMs."
   type        = string
   default     = null
 }
